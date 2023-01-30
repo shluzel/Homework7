242072ea3bae82d8eed429d0127bfbb3fc868d21
Darya <dasha.890@yandex.ru>
2023-01-30 21:33:20 +0500

    первое задание

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.WriteLine("Введите количество строк массива: ");
+int columns = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int rows = Convert.ToInt32 (Console.ReadLine());
+PrintArray(GetArray(columns,rows,-100,100));
+
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
